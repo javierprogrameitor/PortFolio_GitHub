4229b40a059a11340ca7e540e4260d000d380291
javierprogrameitor <javierprogrameitor@gmail.com>
2024-01-07 14:42:45 +0100

    Move
    New move

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+onmouseover =(e)=>{ 
+    let positionX = window.innerWidth;
+    let positionY = window.innerHeight;
+   
+    
+    card1.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card1.style.transition="all 0.5s ease-out";
+ 
+    card2.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card2.style.transition="all 0.5s ease-out";
+
+    card3.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card3.style.transition="all 0.5s ease-out";
+
+    card4.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card4.style.transition="all 0.5s ease-out";
+
+    card5.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card5.style.transition="all 0.5s ease-out";
+
+    card6.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card6.style.transition="all 0.5s ease-out";
+
+    card7.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card7.style.transition="all 0.5s ease-out";
+
+    card8.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card8.style.transition="all 0.5s ease-out";
+
+    card9.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card9.style.transition="all 0.5s ease-out";
+
+    card10.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card10.style.transition="all 0.5s ease-out";
+
+    card11.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card11.style.transition="all 0.5s ease-out";
+
+    card12.style.transform=`rotateY(${(e.clientX/positionX-1)*25}deg) rotateX(${(e.clientY/positionY-1)*25}deg)`;
+    card12.style.transition="all 0.5s ease-out";
+
+
+    }
